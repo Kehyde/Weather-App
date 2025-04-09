b7b0da562b90b6850aaecfb7d759f2cd3396396e
HYDE <hydevosse@gmail.com>
2025-04-09 15:33:39 -0700

    add map feature, add new styling | V.0.1.0

--- weatherDisplay.js
+++ weatherDisplay.js
@@ -1,10 +1,9 @@
 const cityNameDisplay = document.querySelector("#cityNameDisplay");
 const temperatureDisplay = document.querySelector("#tempDisplay");
 const conditionDisplay = document.querySelector("#conditionDisplay");
+const weatherData = JSON.parse(sessionStorage.getItem("weatherData"));
 
 window.onload = function () {
-  const weatherData = JSON.parse(sessionStorage.getItem("weatherData"));
-
   console.log("Weather data on display page:", weatherData);
 
   if (weatherData && weatherData.length > 0) {
@@ -22,6 +21,19 @@ window.onload = function () {
       `;
 
       cityNameDisplay.appendChild(cityWeatherElement);
+
+      const latitude = weatherObject.latitude;
+      const longitude = weatherObject.longitude;
+
+      console.log(latitude, longitude);
+
+      var map = L.map("map").setView([latitude, longitude], 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
     });
   } else {
     console.error("No weather data found.");
